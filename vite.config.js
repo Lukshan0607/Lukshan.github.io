21403045571e9dce58c081444ca309719732b6ea
GeethmiUduwana <lukshansadeepa@gmail.com>
2026-06-09 17:15:42 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Lukshan.github.io/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
